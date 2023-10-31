2e892f42b769bc4929b06a310dbf382a58077639
Gaborojers <gaborojers@gmail.com>
2023-10-31 08:58:01 -0600

    Se subieron nuevos datos

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,43 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root, { loader as rootLoader, action as rootAction, } from "./routes/root";
+import ErrorPage from "./error-page";
+import Contact, { loader as contactLoader } from "./routes/contact";
+import EditContact, { action as editAction, } from "./routes/edit";
+import { action as destroyAction } from "./routes/destroy";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+        loader: contactLoader,
+      },
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact />,
+        loader: contactLoader,
+        action: editAction,
+      },
+      {
+        path: "contacts/:contactId/destroy",
+        action: destroyAction,
+        errorElement: <div>Oops! There was an error.</div>,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,8 +5,35 @@ import Helado from '../assets/img/4534108-removebg-preview.png';
 import MenuLateral from '../components/MenuLateral';
 import MenuCategorías from '../components/MenuCategorías';
 import Imagenes from '../components/Imagenes';
+import Button from 'react-bootstrap/esm/Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus, faMinus } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
+  const [chispasCounts, setChispasCounts] = useState({
+    chispa1: 0,
+    chispa2: 0,
+    chispa3: 0,
+    chispa4: 0,
+    chispa5: 0,
+  });
+
+  const handleIncrement = (chispa) => {
+    setChispasCounts((prevState) => ({
+      ...prevState,
+      [chispa]: prevState[chispa] + 1,
+    }));
+  };
+
+  const handleDecrement = (chispa) => {
+    if (chispasCounts[chispa] > 0) {
+      setChispasCounts((prevState) => ({
+        ...prevState,
+        [chispa]: prevState[chispa] - 1,
+      }));
+    }
+  };
+
 
   return (
       <div>
@@ -28,20 +55,41 @@ function App() {
       <p className="letras"><strong>Nueva</strong> Orden</p>
 
       <div className="chispas">
-                <p className="letra">Chispas</p>
-            </div>
+        <p className="letra">Chispas</p>
+        <Button className='mas' style={{ backgroundColor: 'transparent', border: 'none', color: 'black' }} onClick={() => handleIncrement('chispa1')}>
+          <FontAwesomeIcon icon={faPlus} />
+        </Button>
+        <span className='cifra'>{chispasCounts['chispa1']}</span>
+        <Button className='menos' style={{ backgroundColor: 'transparent', border: 'none', color: 'black' }} onClick={() => handleDecrement('chispa1')}>
+          <FontAwesomeIcon icon={faMinus} />
+        </Button>
+      </div>
 
             <br />
 
-            <div className="chispas">
-                <p className="letra">Chispas</p>
-            </div>
+      <div className="chispas">
+        <p className="letra">Chispas</p>
+        <Button className='mas' style={{ backgroundColor: 'transparent', border: 'none', color: 'black' }} onClick={() => handleIncrement('chispa2')}>
+          <FontAwesomeIcon icon={faPlus} />
+        </Button>
+        <span className='cifra'>{chispasCounts['chispa2']}</span>
+        <Button className='menos' style={{ backgroundColor: 'transparent', border: 'none', color: 'black' }} onClick={() => handleDecrement('chispa2')}>
+          <FontAwesomeIcon icon={faMinus} />
+        </Button>
+      </div>
 
             <br />
 
-            <div className="chispas">
-                <p className="letra">Chispas</p>
-            </div>
+      <div className="chispas">
+        <p className="letra">Chispas</p>
+        <Button className='mas' style={{ backgroundColor: 'transparent', border: 'none', color: 'black' }} onClick={() => handleIncrement('chispa3')}>
+          <FontAwesomeIcon icon={faPlus} />
+        </Button>
+        <span className='cifra'>{chispasCounts['chispa3']}</span>
+        <Button className='menos' style={{ backgroundColor: 'transparent', border: 'none', color: 'black' }} onClick={() => handleDecrement('chispa3')}>
+          <FontAwesomeIcon icon={faMinus} />
+        </Button>
+      </div>
 
             <br />
 
@@ -77,15 +125,29 @@ function App() {
               <br />
 
               <div className="divsChispas">
-              <div className="chispas">
-              <p className="letra">Chispas</p>
-            </div>
+      <div className="chispas">
+        <p className="letra">Chispas</p>
+        <Button className='mas' style={{ backgroundColor: 'transparent', border: 'none', color: 'black' }} onClick={() => handleIncrement('chispa4')}>
+          <FontAwesomeIcon icon={faPlus} />
+        </Button>
+        <span className='cifra'>{chispasCounts['chispa4']}</span>
+        <Button className='menos' style={{ backgroundColor: 'transparent', border: 'none', color: 'black' }} onClick={() => handleDecrement('chispa4')}>
+          <FontAwesomeIcon icon={faMinus} />
+        </Button>
+      </div>
 
              <br />
 
-            <div className="chispas">
-              <p className="letra">Chispas</p>
-            </div>
+      <div className="chispas">
+        <p className="letra">Chispas</p>
+        <Button className='mas' style={{ backgroundColor: 'transparent', border: 'none', color: 'black' }} onClick={() => handleIncrement('chispa5')}>
+          <FontAwesomeIcon icon={faPlus} />
+        </Button>
+        <span className='cifra'>{chispasCounts['chispa5']}</span>
+        <Button className='menos' style={{ backgroundColor: 'transparent', border: 'none', color: 'black' }} onClick={() => handleDecrement('chispa5')}>
+          <FontAwesomeIcon icon={faMinus} />
+        </Button>
+      </div>
               </div>
 
             <div className="botonesAC">
